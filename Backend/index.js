fc4e2e3bff34deaa29808be46b7d764b684504d5
Nandan R <nandanr7284@gmail.com>
2024-11-16 23:39:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,10 @@ import router from './Routers/Routes.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'https://webcook-service-tipx.onrender.com',
+}));
 // app.options('/signup', cors()); // Preflight handling for /signup
 
 // app.set("view engine","ejs");
